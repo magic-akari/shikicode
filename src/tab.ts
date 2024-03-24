80eab1d2e9c6954f5d2724a1d08a7866cdb9b22a
magic-akari <akari.ccino@gmail.com>
2024-03-24 21:37:07 +0800

    fix: indent and outdent

--- src/tab.ts
+++ src/tab.ts
@@ -18,16 +18,19 @@ function indent(input: HTMLTextAreaElement, config: IndentConfig) {
 	const end_at_line_end = end === value_length || value[end] === "\n" || value[end] === "\r";
 	const select_both_ends = start !== end && start_at_line_start && end_at_line_end;
 
+	const block = value.slice(line_start, line_end);
+
 	if (select_both_ends || isMultiLine(value, start, end)) {
-		const replacement = value.slice(line_start, line_end).replaceAll(/^[ \t]*/gm, (leading, offset, str) => {
+		const replacement = block.replaceAll(/^[ \t]*/gm, (leading, offset, str) => {
 			if (str[offset] === "\n" || str[offset] === "\r" || offset === line_end) return leading;
 
-			const tab_count = 1 + ~~(countLeadingSpaces(leading, 0, tabSize) / tabSize);
+			let [tab_width] = countLeadingSpaces(leading, tabSize);
+			tab_width += tabSize - (tab_width % tabSize);
 
 			if (insertSpaces) {
-				return " ".repeat(tab_count * tabSize);
+				return " ".repeat(tab_width);
 			}
-			return "\t".repeat(tab_count);
+			return "\t".repeat(tab_width / tabSize);
 		});
 		input.setRangeText(replacement, line_start, line_end);
 		input.dispatchEvent(new Event("input"));
@@ -36,7 +39,8 @@ function indent(input: HTMLTextAreaElement, config: IndentConfig) {
 
 	let replacement = "\t";
 	if (insertSpaces) {
-		const padding = tabSize - ((start - line_start) % tabSize);
+		let [tab_width] = countLeadingSpaces(block, tabSize);
+		const padding = tabSize - (tab_width % tabSize);
 		replacement = " ".repeat(padding);
 	}
 
@@ -70,13 +74,16 @@ function outdent(input: HTMLTextAreaElement, config: IndentConfig) {
 	const value_slice = value.slice(line_start, line_end);
 
 	const replacement = value_slice.replaceAll(/^[ \t]*/gm, (leading) => {
-		const tab_count = ~~((countLeadingSpaces(leading, 0, tabSize) - tabSize) / tabSize);
-		if (tab_count <= 0) return "";
+		let [tab_width] = countLeadingSpaces(leading, tabSize);
+		tab_width -= 1;
+		tab_width -= tab_width % tabSize;
+
+		if (tab_width <= 0) return "";
 
 		if (insertSpaces) {
-			return " ".repeat(tab_count * tabSize);
+			return " ".repeat(tab_width);
 		}
-		return "\t".repeat(tab_count);
+		return "\t".repeat(tab_width / tabSize);
 	});
 	if (replacement === value_slice) return;
 	input.setRangeText(replacement, line_start, line_end);
@@ -115,25 +122,31 @@ function getLineEnd(value: string, index: number) {
 }
 
 function isMultiLine(value: string, start: number, end: number) {
-	for (let i = start; i < end; i++) {
-		if (value[i] === "\n") {
-			return true;
-		}
-	}
-	return false;
+	return value.slice(start, end).includes("\n");
 }
 
-function countLeadingSpaces(value: string, start: number, tabSize: number) {
-	let count = 0;
-	for (let i = start; i < value.length; i++) {
-		if (value[i] === " ") {
-			count++;
-		} else if (value[i] === "\t") {
-			count += tabSize;
-		} else {
-			break;
+function countLeadingSpaces(value: string, tabSize: number): [width: number, end: number] {
+	let width = 0;
+	let end = 0;
+
+	while (end < value.length) {
+		switch (value[end]) {
+			case "\t": {
+				width += tabSize - (width % tabSize);
+				end++;
+				break;
+			}
+			case " ": {
+				width++;
+				end++;
+				break;
+			}
+
+			default: {
+				return [width, end];
+			}
 		}
 	}
 
-	return count;
+	return [width, end];
 }
