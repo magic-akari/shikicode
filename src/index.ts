58b0c74228a157011c52b6624c87a7cc8ed9bcaf
magic-akari <akari.ccino@gmail.com>
2024-03-24 10:39:42 +0800

    chore: improve tree shaking

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as shiki from "shiki";
+import { getHighlighter } from "shiki";
 
 import type { ICodeEditor, InitOptions } from "./core.js";
 import { createWithShiki } from "./core.js";
@@ -6,5 +6,5 @@ import { createWithShiki } from "./core.js";
 export * from "./core.js";
 
 export async function create(domElement: HTMLElement, options?: InitOptions): Promise<ICodeEditor> {
-	return createWithShiki(shiki, domElement, options);
+	return createWithShiki({ getHighlighter }, domElement, options);
 }
